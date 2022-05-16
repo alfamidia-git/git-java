99bff1276fe11565275c6a3a85305f25295249c8
Marlon Nunes <marlon@alfamidia.com.br>
2022-05-16 19:52:12 -0300

    Adicionado a lista e o print da lista

--- src/Principal.java
+++ src/Principal.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Olá mundo");
+		
+		List<String> lista = new ArrayList<>();
+		
+		lista.add("bla bla bla");
+		lista.add("bla bla bla2");
+		
+		for(String item : lista) {
+			System.out.println(item);
+		}
 	}
 
 }
